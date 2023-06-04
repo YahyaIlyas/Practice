723ac0a304140b2e0fd5a433f21458c7a78525cb
Yahya ilyas <Yahyailyas@gmail.com>
2023-06-05 00:02:12 +0500

    commit

---
+++ java.js
@@ -0,0 +1,41 @@
+// const yahya = 7
+// const ilyas = 5
+// result = yahya + ilyas
+// console.log(result)
+// let a = "yahya"
+// document.write(typeof a)
+// let item = {
+//     "yahya":true,
+// }
+// let a = "yahya";
+// let b = 9
+// result= a + b
+// document.write(result)
+
+// document.write(typeof (a+b))
+// const dic ={
+// yahya: "ilyas",
+// bbkuu: "asdj"
+// }
+// document.write(dic.bbkuu)
+// let a =45;
+// let b= 5;
+// let result = a != b
+// // console.log(result)
+// let a = ("hello")
+// alert(a)
+// a = Number.parseInt(a)
+// alert(typeof a)
+let a = prompt("whats your age")
+if(a<0){
+    alert("this is not valid");
+}
+else if(a<18){
+    alert("this is  valid")
+}
+else if(a>18 && a>=17){
+    alert("this is good valid")
+}
+else{
+    alert("this is awsm valid")
+}
